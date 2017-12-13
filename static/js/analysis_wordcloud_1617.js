f0c2cef743303cb36a4ec52cbd8513f7eec93523
YeEun Jang <to.yeeunjang@gmail.com>
2017-12-13 17:29:10 +0900

    add wordcloud

---
+++ static/js/analysis_wordcloud_1617.js
@@ -0,0 +1,40 @@
+var weight = 0.0017,   // changed
+    width = 500, // changed
+    height =500;
+var fill = d3.scaleOrdinal(d3.schemeCategory20);
+d3.csv("data/count_noun_1617.csv", function(d) {
+    return {
+      text: d.word,
+      size: +d.freq * weight
+    }
+  },
+
+  function(data) {
+    data.sort(function(a, b) { return b.size - a.size})
+    data = data.slice(0, 300) // changed
+    d3.layout.cloud().size([width, height]).words(data)
+      //.rotate(function() { return ~~(Math.random() * 2) * 90; })
+      .rotate(0)
+      .font('Godo')
+      .fontSize(function(d) { return d.size; })
+      .on("end", draw)
+      .start();
+    function draw(words) {
+      d3.select("#cloud").append("svg")
+          .attr("width", width)
+          .attr("height", height)
+        .append("g")
+          .attr("transform", "translate(" + width/2 + "," + height/2 + ")")
+        .selectAll("text")
+          .data(words)
+        .enter().append("text")
+          .style("font-size", function(d) { return d.size + "px"; })
+          .style("font-family", 'Godo')
+          .style("fill", function(d, i) { return fill(i); })
+          .attr("text-anchor", "middle")
+          .attr("transform", function(d) {
+            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+          })
+        .text(function(d) { return d.text; });
+    }
+  });
